eeb4a2cfe679d1136508575d15911c73ece52902
Othmani Ali <othmanixali@gmail.com>
2018-03-08 19:40:22 +0100

    keys and space for scrolling the page are disabled

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
