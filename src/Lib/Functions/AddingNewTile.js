0ae0138c529527105308aa3c2f1a1dd493ef63c1
Othmani Ali <othmanixali@gmail.com>
2018-03-10 15:41:01 +0100

    Code Improvement fro addingNewTile function

--- src/Lib/Functions/AddingNewTile.js
+++ src/Lib/Functions/AddingNewTile.js
@@ -2,12 +2,12 @@ import print from './Print';
 
 const addingNewTile = (matrix, idStore) => {
     print('addingNewTile',3);
-    let emptytiles =  matrix.map( // collect positions ofcountEmpty tiles in a matrix form
+    let emptytiles =  matrix.map( // collect positions of Empty tiles in a matrix form
             (matrixRow,row) => matrixRow.reduce(  
             (list,tile,col) => !tile.normal.val? list.concat([ [row,col] ]):list, []));
-    // transformcountEmptyTiles from a matrix form to an array
-    emptytiles = [...emptytiles[0],...emptytiles[1],...emptytiles[2],...emptytiles[3]];
-    // pick a randomcountEmptyTile position from emptiTiles
+    // transform 'emptyTiles' from a matrix form to an array
+    emptytiles = emptytiles.reduce( (array,rowOfEmptytiles) => array.concat(rowOfEmptytiles),[]);
+    // pick a randomcountEmptyTile position from emptyTiles
     const randomEmptyTile = emptytiles[Math.floor(emptytiles.length*Math.random())] ;
     const row0 = randomEmptyTile[0], col0 = randomEmptyTile[1];
     //const M =  Array(4).fill(0).map( (_,row) => Array(4).fill(0).map( (_,col) => Object.assign({}, matrix[row][col]))); 
