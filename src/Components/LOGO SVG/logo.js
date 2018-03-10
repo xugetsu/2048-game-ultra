919d2c23ba5b8bbe1e963aef53b399d04e306b45
Othmani Ali <othmanixali@gmail.com>
2018-03-10 22:48:15 +0100

    logo code improvement

--- src/Components/LOGO SVG/logo.js
+++ src/Components/LOGO SVG/logo.js
@@ -4,22 +4,21 @@ const logo = () => {
     const style1 = {stroke:'rgba(125,185,232,1)',strokeWidth:.5};
     const style2 = {fill:'rgba(41,137,216,1)',stroke:'rgba(41,137,216,1)',strokeWidth:1};
 
-    const scale = 1;
-    const height = 170*scale*scale;
-    const thikness = height/5;
-    const width = height*1.2*scale;
 
-    const x0 = 20;
-    const y0 = 30; 
-    const x1 = x0;
-    const y1 = y0 + 190;
-    const x3 = x0;
-    const y3 = y0 + 450;
+
+    const h = 100;
+    
+    const height = h/2.825; 
+    const thikness = height/5;
+    const width = h/2.825*1.2;
+    const x0 = 0, y0 = 0; 
+    const x1 = x0, y1 = y0 + height*1.11765;
+    const x3 = x0, y3 = y0 + height*2.825;
 
     const poly1 = [[x1, y1], 
-                   [x0, y1 + thikness*0.8 ], 
-                   [x1 + width, y1 + 50 + thikness*0.8], 
-                   [x1 + width, y1 + 50 ]]; // < "/" >
+                   [x0, y1 + thikness*0.6 ], 
+                   [x1 + width, y1 + height*7/17 + thikness*0.6], 
+                   [x1 + width, y1 + height*7/17  ]]; // < "/" >
 
     const poly2 =  [[x0 + width/2, thikness + y0], 
                     [x0, y0 + height], 
@@ -35,7 +34,7 @@ const logo = () => {
                     [x3 + width, y3 - height]]; // < / ">"                    
 
     return (
-    <svg height="500" width="250">
+    <svg height={h} width={width}>
         <polygon points={poly1} style={style2}/>
         <polygon points={poly2} style={style2}/>
         <polygon points={poly3} style={style2}/>
