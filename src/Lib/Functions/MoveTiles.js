2df897d1257a9ffe84cf25ce3ddc7220c7d0785d
Othmani Ali <othmanixali@gmail.com>
2018-03-10 15:15:50 +0100

    Code Improvement for moveTiles function

--- src/Lib/Functions/MoveTiles.js
+++ src/Lib/Functions/MoveTiles.js
@@ -4,12 +4,15 @@ import normaltileObject from './NormaltileObject';
 
 const moveTiles = (matrix,direction) => {
     print('moveTiles',3);
-     const newMatrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    const N = matrix.length;
+    // martix with size N
+    const newMatrix = Array(N).fill(0).map( (_,row) => Array(N).fill(0)); 
+
     switch (direction) {
         case 'left':   
-            for( let r = 0; r < 4; r++ ){
-                let j = 0, k = 3;
-                for( let c = 0; c < 4; c++ ){
+            for( let r = 0; r < N; r++ ){
+                let j = 0, k = N-1;
+                for( let c = 0; c < N; c++ ){
                     const tn = matrix[r][c].normal;
                     if ( tn.val ){  const tm = matrix[r][c].merged;
                     newMatrix[r][j] = tm ? mergedtileObject(tn.id ,tn.val, j ,tn.row,tn.newTile, tm.id,tm.val, j ,tm.row)
@@ -21,9 +24,9 @@ const moveTiles = (matrix,direction) => {
          
         return newMatrix;
         case 'right':
-            for( let r = 0; r < 4; r++ ){
-                let j = 0, k = 3;
-                for( let c = 3; c > -1 ; c-- ){
+            for( let r = 0; r < N; r++ ){
+                let j = 0, k = N-1;
+                for( let c = N-1; c > -1 ; c-- ){
                     const tn = matrix[r][c].normal;
                     if ( tn.val ){  const tm = matrix[r][c].merged;
                     newMatrix[r][k] = tm ? mergedtileObject(tn.id ,tn.val, k , r ,tn.newTile, tm.id,tm.val, k , r )
@@ -34,9 +37,9 @@ const moveTiles = (matrix,direction) => {
             }}
         return newMatrix;
         case 'down': 
-            for( let c = 0; c < 4; c++ ){
-                let j = 0, k =3;
-                for( let r = 3; r > -1; r-- ){
+            for( let c = 0; c < N; c++ ){
+                let j = 0, k =N-1;
+                for( let r = N-1; r > -1; r-- ){
                     const tn = matrix[r][c].normal;
                     if ( tn.val ){  const tm = matrix[r][c].merged;
                     newMatrix[k][c] = tm ? mergedtileObject(tn.id ,tn.val, c , k ,tn.newTile, tm.id,tm.val, c , k )
@@ -47,9 +50,9 @@ const moveTiles = (matrix,direction) => {
             }}
         return newMatrix;
         case 'up':  
-            for( let c = 0; c < 4; c++ ){
-                let j = 0, k = 3;
-                for( let r = 0; r < 4; r++ ){
+            for( let c = 0; c < N; c++ ){
+                let j = 0, k = N-1;
+                for( let r = 0; r < N; r++ ){
                     const tn = matrix[r][c].normal;
                     if ( tn.val ){  const tm = matrix[r][c].merged;
                     newMatrix[j][c] = tm ? mergedtileObject(tn.id ,tn.val, c , j ,tn.newTile, tm.id,tm.val, c , j )
