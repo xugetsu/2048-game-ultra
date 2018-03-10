c64ad9e7cb0ff3e3589b9cab53f680e84cd54b35
Othmani Ali <othmanixali@gmail.com>
2018-03-10 22:54:52 +0100

    changes in GameInfo.js

--- src/Components/GameInfo/GameInfo.js
+++ src/Components/GameInfo/GameInfo.js
@@ -2,30 +2,28 @@ import React from 'react';
 import style from './GameInfo.css'
 const gameInfo = (props) => {
     return (
-        <div className={style.GameInfoWrapper}>
-            <div className={style.GameInfo}>
-                <header className={style.AppHeader}>
-                    <div className={style.AppTitle}>
-                        <h1>2048</h1>
-                        <p>
-                            <b>Play 2048 Game Online</b>
-                            <br />
-                            Join the numbers and get to the 
-                            <b> 2048 tile!</b>
-                        </p>
+        <div className={style.GameInfo}>
+            <div className={style.AppHeader}>
+                <div className={style.AppTitle}>
+                    <h1>2048</h1>
+                    <p>
+                        <b>Play 2048 Game Online</b>
+                        <br />
+                        Join the numbers and get to the 
+                        <b> 2048 tile!</b>
+                    </p>
+                </div>
+                <div className = {style.Info_wrapper}>
+                    <div className = {style.Info_Score}> 
+                        {props.score}
                     </div>
-                    <div className = {style.Info_wrapper}>
-                        <div className = {style.Info_Score}> 
-                            {props.score}
-                        </div>
-                        <div className = {style.Info_Best}> 
-                            {props.score}
-                        </div>
-                            <div className = {style.NewGame} onClick = {props.newGame}> 
-                            <p>New Game</p>
-                        </div>
-                    </div>   
-                </header>
+                    <div className = {style.Info_Best}> 
+                        {props.score}
+                    </div>
+                        <div className = {style.NewGame} onClick = {props.newGame}> 
+                        <p>New Game</p>
+                    </div>
+                </div>
             </div>
          </div> 
     );
