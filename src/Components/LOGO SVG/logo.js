7f6e5596c5b00a417ad57ee558293420123dfbf9
Othmani Ali <othmanixali@gmail.com>
2018-03-11 04:36:29 +0100

    logo modified

--- src/Components/LOGO SVG/logo.js
+++ src/Components/LOGO SVG/logo.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
 const logo = () => {
-    const style1 = {stroke:'rgba(125,185,232,1)',strokeWidth:.5};
+    const style1 = {fill:'#333',stroke:'#333',strokeWidth:.5};
     const style2 = {fill:'rgba(41,137,216,1)',stroke:'rgba(41,137,216,1)',strokeWidth:1};
+    const style3 = {fill:'white',stroke:'white',strokeWidth:1};
+    
 
-
-
-    const h = 100;
+    const h = 250;
     
     const height = h/2.825; 
     const thikness = height/5;
     const width = h/2.825*1.2;
     const x0 = 0, y0 = 0; 
-    const x1 = x0, y1 = y0 + height*1.11765;
-    const x3 = x0, y3 = y0 + height*2.825;
+    const x1 = x0, y1 = y0 + height*1.11765, y11 = y0 + height*1.11765 - h/500*130;
+    const y12 = y0 + thikness/2; 
+    const x3 = x0, y3 = y0 + height*2.825 ;
 
     const poly1 = [[x1, y1], 
-                   [x0, y1 + thikness*0.6 ], 
-                   [x1 + width, y1 + height*7/17 + thikness*0.6], 
+                   [x1, y1 + thikness*0.8 ], 
+                   [x1 + width, y1 + height*7/17 + thikness*0.8], 
                    [x1 + width, y1 + height*7/17  ]]; // < "/" >
+    const poly11 = [[x1 + h/25, y11], 
+                   [x1, y11 + thikness*0.8 ], 
+                   [x1 + width + h/25, y11 + height*4/17 + thikness*0.8], 
+                   [x1 + width , y11 + height*4/17  ]]; // < "/" >
 
     const poly2 =  [[x0 + width/2, thikness + y0], 
                     [x0, y0 + height], 
@@ -26,6 +31,14 @@ const logo = () => {
                     [x0 + width/2, y0], 
                     [x0 + width, y0 + height - thikness], 
                     [x0 + width, y0 + height]]; // "<" / >
+
+    const poly21 =  [[x0 + width/2, thikness + y12], 
+                    [x0, y12 + height], 
+                    [x0, y12 + height - 1.2*thikness],
+                    [x0 + width/2, y12], 
+                    [x0 + width, y12 + height - thikness/2.2], 
+                    [x0 + width, y12 + height]]; // "<<" / >
+
     const poly3 =  [[x3 + width/2, - thikness + y3], 
                     [x3, y3 - height], 
                     [x3, y3 - height + thikness],
@@ -34,12 +47,15 @@ const logo = () => {
                     [x3 + width, y3 - height]]; // < / ">"                    
 
     return (
-    <svg height={h} width={width}>
-        <polygon points={poly1} style={style2}/>
+    <svg height={h} width={width+h/25}>
         <polygon points={poly2} style={style2}/>
-        <polygon points={poly3} style={style2}/>
+        <polygon points={poly11} style={style3}/>
+        <polygon points={poly21} style={style2}/>
+
     </svg>
     );
 }
 
+//<polygon points={poly3} style={style2}/>
+//<polygon points={poly1} style={style3}/>
 export default logo;
