f1b37329bb8e16224c285052c741d3b8debcc12c
Othmani Ali <othmanixali@gmail.com>
2018-03-12 10:52:46 +0100

    Adding a Game menu component

---
+++ src/Components/GameMenu/GameMenu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './GameMenu.css'
+
+const gameMenu = (props) => {
+    return (
+        <div className={style.GameMenuWrapper}> 
+ 
+ <div className={style.GameMenu}>
+        <ul>
+                <li onClick = {props.resizeMatrix}>
+                    <svg viewBox="0 0 500 500">
+                      <path d="M0   32v128h128V32H0zm120 
+                                    120H8V40h112v112zm40-120v128h128V32H160zm120 
+                                    120H168V40h112v112zm40-120v128h128V32H320zm120 
+                                    120H328V40h112v112zM0 192v128h128V192H0zm120 
+                                    120H8V200h112v112zm40-120v128h128V192H160zm120 
+                                    120H168V200h112v112zm40-120v128h128V192H320zm120 
+                                    120H328V200h112v112zM0 352v128h128V352H0zm120 
+                                    120H8V360h112v112zm40-120v128h128V352H160zm120 
+                                    120H168V360h112v112zm40-120v128h128V352H320z"
+                      /> 
+                    </svg>
+                </li>
+                <li> </li>
+            </ul>
+        </div>
+ 
+        </div>
+    );
+}
+export default gameMenu;
