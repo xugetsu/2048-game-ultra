5d8f32eba0e92463e22808e19486093d95c868f6
Othmani Ali <othmanixali@gmail.com>
2018-03-14 00:01:01 +0100

    Updating the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,25 @@
 
 import React from 'react';
-import Logo from './MyLogo/MyLogo';
+import Logo from './Svgs/MyLogo';
 import style from './Footer.css';
+import LinkedIn from './Svgs/LinkedIn';
+import Github from './Svgs/Github';
 
 const footer = (props) => {
     return (
         <footer>
-            <div className = {style.LogoFooter}>
-                <Logo logoHeight = {150}/>
+            <div>
+                This project is made by 
+                <a href="https://www.linkedin.com/in/ali-othmani-11873707" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                > Ali Othmani</a>
             </div>
-            This project is made by 
-            <a href="https://www.linkedin.com/in/ali-othmani-11873707" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            > Ali Othmani</a>
+            
+            <div className = {style.Logos}> <LinkedIn /></div>
+            <div className = {style.Logos}> <Github /></div>
+            <div className = {style.Logos}> <Logo logoHeight = {120}/> </div>
+            <div className = {style.FollowMe}><h1>Follow me</h1></div>
         </footer>
             );
 }
