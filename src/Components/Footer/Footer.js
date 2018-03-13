b997404d84918e930477719c068bee79b524f2ac
Othmani Ali <othmanixali@gmail.com>
2018-03-14 00:45:52 +0100

    Updating the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,17 +8,35 @@ import Github from './Svgs/Github';
 const footer = (props) => {
     return (
         <footer>
-            <div>
-                This project is made by 
-                <a href="https://www.linkedin.com/in/ali-othmani-11873707" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                > Ali Othmani</a>
+            <div className = {style.LastWords}  >
+               <p> This project is made by 
+                    <a> Ali Othmani </a>
+                    Using the JavaScript Library React 
+                </p>
+                <a href="https://reactjs.org/" style = {{margin:'10px 0 0 45px ', float:'left'}}
+                   target="_blank" 
+                   rel="noopener noreferrer">
+                  <img src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" 
+                  alt="" height="50" />
+                </a>
+
             </div>
             
-            <div className = {style.Logos}> <LinkedIn /></div>
-            <div className = {style.Logos}> <Github /></div>
-            <div className = {style.Logos}> <Logo logoHeight = {120}/> </div>
+            <div className = {style.Logos}>
+               <a href="https://linkedin.com/in/ali-othmani-11873707/" target="_blank" rel="noopener noreferrer">
+                     <LinkedIn />
+               </a>
+            </div>
+            <div className = {style.Logos}>
+               <a href="https://github.com/xugetsu" target="_blank" rel="noopener noreferrer">
+                     <Github />
+               </a>
+            </div>            
+            <div className = {style.Logos}>
+               <a href="/" target="_blank" rel="noopener noreferrer">
+                     <Logo  logoHeight = {120}/>
+               </a>
+            </div>            
             <div className = {style.FollowMe}><h1>Follow me</h1></div>
         </footer>
             );
