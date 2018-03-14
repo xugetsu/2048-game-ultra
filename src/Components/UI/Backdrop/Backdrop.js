03ad543c5bfd0c5495981ca58606e144f51d670d
Othmani Ali <othmanixali@gmail.com>
2018-03-14 15:03:02 +0100

    fixing the backdrop componenet

--- src/Components/UI/Backdrop/Backdrop.js
+++ src/Components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,9 @@ import style from './Backdrop.css';
 
 
 const backdrop = (props) => (
-     <div className={style.Backdrop}>
+     <div className={style.Backdrop} 
+            style={{width:props.matrixSize + 'px', 
+                    height: props.matrixSize + 'px'}} >
         <h1> Game Over </h1>
         <h3> Your Score is : {props.score} </h3>
         <div className = {style.NewGame} 
