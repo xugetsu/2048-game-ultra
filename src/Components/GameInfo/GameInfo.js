dc1207eedbf41f8fb2da7282f2a356c28c3e49b3
Othmani Ali <othmanixali@gmail.com>
2018-03-14 15:11:32 +0100

    Updating GameInfo component

--- src/Components/GameInfo/GameInfo.js
+++ src/Components/GameInfo/GameInfo.js
@@ -2,16 +2,14 @@ import React from 'react';
 import style from './GameInfo.css'
 const gameInfo = (props) => {
     return (
-        <div className={style.GameInfo}>
-                <div className = {style.Info_wrapper}>
-                    <div className = {style.Info_Score}> 
-                        {props.score}
-                    </div>
-                    <div className = {style.Info_Best}> 
-                        {props.score}
-                    </div>  
+            <div className = {style.Info_wrapper}>
+                <div className = {style.Info_Score}> 
+                    {props.score}
                 </div>
-        </div>
+                <div className = {style.Info_Best}> 
+                    {props.score}
+                </div>  
+            </div>   
     );
 }
 
