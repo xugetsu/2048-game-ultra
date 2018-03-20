fd3ecb8897d29d5ab48fbc202f2b0053e07ab36e
Othmani Ali <othmanixali@gmail.com>
2018-03-20 22:23:00 +0100

    Adding instruction in info box

--- src/Components/GameInfo/GameInfo.js
+++ src/Components/GameInfo/GameInfo.js
@@ -4,7 +4,7 @@ const gameInfo = (props) => {
     return (
             <div className = {style.Info_wrapper}>
                 <div className = {style.Tips}> 
-                   Info: 
+                   <p><span>Info:</span> Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!</p>
                 </div> 
                 <div className = {style.Info_Score}> 
                     {props.score}
