86a35f85475820031d5486e1863b0ae97eee066d
Othmani Ali <othmanixali@gmail.com>
2018-03-20 21:49:53 +0100

    Deleting Unnecessary comments

--- src/Containers/MatrixContainer/MatrixContainer.js
+++ src/Containers/MatrixContainer/MatrixContainer.js
@@ -2,16 +2,10 @@ import React from 'react';
 import style from './MatrixContainer.css';
 import Grids from '../../Components/Grids/Grids';
 import Tiles from '../../Components/Tiles/Tiles';
-import MergedTiles from '../../Components/Tiles/MergedTiles';
 import Backdrop from '../../Components/UI/Backdrop/Backdrop';
 
 const matrixContainer = (props) => {
     const N = props.matrix.length;
-//     const gridSize = [80,72,60,55]; 
-//     const gridMargins = [13,12,10,9]; 
-//     const fontSizes = [45,40,35,33];
-//     const lineHeight = [85,75,65,60];                          
-//     const matrixSize = gridSize[N-4]*N + gridMargins[N-4]*(N + 1) ;
     const backdrop = props.gameOver ? <Backdrop  newGame = {props.newGame} 
                                                  score   = {props.score}
                                                  matrixSize =  {props.sizes.matrixSize}  /> 
@@ -36,13 +30,4 @@ const matrixContainer = (props) => {
         </div> 
     );
 } 
-export default matrixContainer;
-
-/*
-        <MergedTiles matrix       = {props.matrix} 
-                    tileSize     = {gridSize[N-4]} 
-                    gridMargin   = {gridMargins[N-4]}
-                    fontSize     = {fontSizes[N-4]}                           
-                    lineHeight   = {lineHeight[N-4]}/>
-*/
-                     
+export default matrixContainer;             
