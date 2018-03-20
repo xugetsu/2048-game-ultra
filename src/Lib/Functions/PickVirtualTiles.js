7dec73fc5fd2813c745ea5e574f4efb481c3f04d
Othmani Ali <othmanixali@gmail.com>
2018-03-20 21:49:01 +0100

    Bug Fix in PickVirtualTiles.js

--- src/Lib/Functions/PickVirtualTiles.js
+++ src/Lib/Functions/PickVirtualTiles.js
@@ -3,7 +3,7 @@ import print from './Print';
 const pickVirtualTiles = (matrix) => { 
     print('pickVirtualTiles',3);
     return matrix.map( 
-    (matrixRow,r) => matrixRow.reduce((list, tile) => tile.merged? list.concat({normal:tile.merged}) : list, []))
+    (matrixRow,r) => matrixRow.reduce((list, tile) => tile.merged? list.concat({normal:tile.merged, merged:tile.merged}) : list, []))
     .reduce( (list,listRow) => listRow[0]? list.concat(listRow) : list, [])};
 
  export default pickVirtualTiles;
