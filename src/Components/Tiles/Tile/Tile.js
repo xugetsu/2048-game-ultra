cc45a7287d917f08bb4b7848cb4196f20b07dd84
Othmani Ali <othmanixali@gmail.com>
2018-03-20 21:48:19 +0100

    Optimising Tiles.js and Tile.js Components

--- src/Components/Tiles/Tile/Tile.js
+++ src/Components/Tiles/Tile/Tile.js
@@ -2,34 +2,11 @@ import React from 'react';
 import style from './Tile.css';
 
 const tile = (props) => {
-    
- 
-    const innerTile_styling = {
-                    width: props.tileSize +'px',
-                    height: props.tileSize +'px',
-                    fontSize: props.fontSize +'px',
-                    lineHeight:props.lineHeight +'px'
-                  }; 
-    const distance = props.tileSize + props.gridMargin;                
-    const x = (distance * props.col) + 0.5* props.gridMargin + 'px';
-    const y = (distance * props.row) + 0.5* props.gridMargin + 'px';
-    const tile_styling = {
-                    width: props.tileSize +'px',
-                    height: props.tileSize +'px',
-                    fontSize: props.fontSize +'px',
-                    lineHeight: props.lineHeight +'px',
-                    transform: 'translate('+[x,y]+')'
-                  };  
-    const styles = [   style.Tile,
-                       style['Tile_' + props.val],
-                       props.new ? style.TileNew : '',
-                       props.removeModeState? style.RemoveTile : ''
-                    ]; 
-
-
     return (
-        <div className={styles.join(' ')} style={tile_styling} onClick = {() => props.removeTile(props.row, props.col)}>
-            <div className={style.TileInner} style ={innerTile_styling} >{props.val? props.val : ''}</div>
+        <div onClick = {props.removeTile} className={props.styles.join(' ')} style={{...props.tile_innerTile_styling,...props.transform_styling}} >
+            <div className={style.TileInner}  style ={props.tile_innerTile_styling} >
+                {props.val? props.val : ''}
+            </div>
         </div>
     );
 };
--- src/Components/Tiles/Tiles.js
+++ src/Components/Tiles/Tiles.js
@@ -1,49 +1,56 @@
 import React from 'react';
+//import createFragment from 'react-addons-create-fragment';
 import style from './Tiles.css';
+import tileStyle from './Tile/Tile.css';
 import Tile from './Tile/Tile';
-import MergedTile from './Tile/MergedTile';
 
 const tiles = (props) => {
-    const virtualTiles = props.virtualTiles;
+    const virtualTiles = props.virtualTiles; // Array of virtual tiles
     const M = props.matrix;
+    // Prepare a list of normal tiles 
     let arrayOfTiles = M.reduce( (list,MRow) => list.concat(MRow) , []); 
+    // Add virtual tiles from virtualTiles array
     arrayOfTiles = virtualTiles? arrayOfTiles.concat(virtualTiles) : arrayOfTiles;
-    const rearrangedTiles = Array(100).fill() // creating an array of tiles arranged by their ids in ascending order   
+    // Re-arrange the list by their ids in ascending order 
+    const rearrangedTiles = Array(100).fill()   
                                      .map( (_,index) => arrayOfTiles.find( el => el.normal.id === index ) )
-                                     .filter( tile => tile !== undefined && tile.normal.val !== 0); // removing places with no matching id                      
-    const componentList = rearrangedTiles.map( tile => 
-         <Tile  key={tile.normal.id.toString()} 
-                val={tile.normal.val} 
-                row={tile.normal.row} 
-                col={tile.normal.col} 
-                new={tile.normal.newTile} 
-                tileSize   = {props.tileSize}
-                gridMargin = {props.gridMargin}
-                fontSize = {props.fontSize}                           
-                lineHeight   = {props.lineHeight}
-                removeTile = {(i,j) => props.removeTile(i, j)}
-                removeModeState = {props.removeModeState} />
-    );
+                                      .filter( tile => tile !== undefined && tile.normal.val !== 0); // removing places with no matching id                      
+    
     const size = (props.tileSize + props.gridMargin)*(props.matrix.length);
-// -----------------------------------------------------------------------------------------
-    const tiles = props.matrix.map( (matrixRow,row) => matrixRow.map(
-        (tile, col) => tile.merged? <MergedTile key = {Math.random().toString()}
-                                                val = {tile.normal.val*2}
-                                                row = {tile.normal.row}
-                                                col = {tile.normal.col}
-                                                tileSize   = {props.tileSize}
-                                                gridMargin = {props.gridMargin}
-                                                fontSize = {props.fontSize}                           
-                                                lineHeight   = {props.lineHeight}/>
-                                :null
-                            ));           
-    const arrayTiles = tiles.reduce( (list,tilesRow) => list.concat(tilesRow) , []); 
-// -----------------------------------------------------------------------------------------
+    const tile_innerTile_styling = {
+                                    width: props.tileSize + 'px',
+                                    height: props.tileSize + 'px',
+                                    fontSize: props.fontSize + 'px',
+                                    lineHeight: props.lineHeight + 'px'
+                                    }; 
+    const distance = props.tileSize + props.gridMargin; 
+
+    // Create a List of Tile components
+    const componentList = rearrangedTiles.map(
+        tile => {  
+                    const x = (distance * tile.normal.col) + 0.5 * props.gridMargin + 'px';
+                    const y = (distance * tile.normal.row) + 0.5 * props.gridMargin + 'px';
+                    const transform_styling = {transform: 'translate('+ [x, y] +')'}; 
+                    let tileStyles = [tileStyle.Tile,  
+                                      tileStyle['Tile_' + (tile.merged ? tile.normal.val*2:tile.normal.val)],
+                                      tile.merged ? tileStyle.TileMerged : '',
+                                      !tile.merged && tile.normal.newTile ? tileStyle.TileNew : '',
+                                      !tile.merged && props.removeModeState? tileStyle.RemoveTile : ''];
+
+                    return (
+                        <Tile   key={tile.normal.id.toString()} 
+                                val={tile.merged ? tile.normal.val*2 : tile.normal.val} 
+                                styles={tileStyles}
+                                transform_styling={transform_styling}
+                                tile_innerTile_styling = {tile_innerTile_styling}
+                                removeTile = {() => props.removeTile(tile.normal.row, tile.normal.col)}/>
+                    );
+                }
+    );
 
     return ( 
     <div className={style.TilesContainer} style = {{height:size,width:size}}  >       
        {componentList}
-       {arrayTiles}
      </div>
     );
 };
