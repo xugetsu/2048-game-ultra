90ba913d30a6b5fc4764813e8c1b55a08a385eeb
Othmani Ali <othmanixali@gmail.com>
2018-04-09 15:40:12 +0100

    imp gameTitle in header cmp

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import style from './Header.css'
+import GameTitle from './GameTitle/GameTitle';
 
 const header = (props) => {
     return (
         <header>
-                <h1>2048 <span>Ultra!</span></h1>
+                <GameTitle />
                 <div className={style.GameIntro}>
-                <p>
-                    <b>Play 2048 Game Online</b>
-                    <br />
-                    Join the numbers and get to the 
-                    <b> 2048 tile!</b>
-                </p>
-            </div>
+                    <p>
+                        <b>Play 2048 Game Online</b>
+                        <br />
+                        Join the numbers and get to the 
+                        <b> 2048 tile!</b>
+                    </p>
+                </div>
          </header>
     );
 }
