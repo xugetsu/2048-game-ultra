a2e66009ee528bb012a0be62bf77ae382c5e26be
Othmani Ali <othmanixali@gmail.com>
2018-04-10 19:37:23 +0100

    create TopPlayers cmp

---
+++ src/Components/GameInfo/TopPlayers/TopPlayers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './TopPlayers.css';
+import TopSVG from './TopSVG/TopSVG';
+
+const topPlayers = (props) => {
+    return (
+            <div className = {styles.TopPlayers} >
+                <div className={styles.TopSVG}><TopSVG /></div>
+            </div>
+
+
+    );
+};
+
+export default topPlayers;
