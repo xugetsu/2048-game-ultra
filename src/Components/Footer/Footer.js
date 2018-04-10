f1e1dc6a1fb6fe5c02771e39435d9fd6fb2926b3
Othmani Ali <othmanixali@gmail.com>
2018-04-10 01:18:36 +0100

    M style in Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,34 +10,35 @@ const footer = (props) => {
         <footer>
             <div className = {style.LastWords}  >
                <p> This project is made by 
-                    <a> Ali Othmani </a>
-                    Using the JavaScript Library React 
+                    <span> Ali Othmani </span>
+                    using the JavaScript Library React 
                 </p>
-                <a href="https://reactjs.org/" style = {{margin:'10px 0 0 45px ', float:'left'}}
+                <a href="https://reactjs.org/" style = {{margin:'10px 0 0 70px ', float:'left'}}
                    target="_blank" 
                    rel="noopener noreferrer">
                   <img src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" 
-                  alt="" height="50" />
+                  alt="" height="35" />
                 </a>
-
             </div>
-            
-            <div className = {style.Logos}>
-               <a href="https://linkedin.com/in/ali-othmani-11873707/" target="_blank" rel="noopener noreferrer">
-                     <LinkedIn />
-               </a>
+            <div className = {style.FollowMe}>
+                <p>Follow me</p>
+                <div className = {style.Logos}>
+                <a href="https://linkedin.com/in/ali-othmani-11873707/" target="_blank" rel="noopener noreferrer">
+                        <LinkedIn />
+                </a>
+                </div>
+                <div className = {style.Logos}>
+                <a href="https://github.com/xugetsu" target="_blank" rel="noopener noreferrer">
+                        <Github />
+                </a>
+                </div>            
+                <div className = {style.Logos}>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                        <Logo  logoHeight = {120}/>
+                </a>
+                </div>            
+                
             </div>
-            <div className = {style.Logos}>
-               <a href="https://github.com/xugetsu" target="_blank" rel="noopener noreferrer">
-                     <Github />
-               </a>
-            </div>            
-            <div className = {style.Logos}>
-               <a href="/" target="_blank" rel="noopener noreferrer">
-                     <Logo  logoHeight = {120}/>
-               </a>
-            </div>            
-            <div className = {style.FollowMe}><h1>Follow me</h1></div>
         </footer>
             );
 }
