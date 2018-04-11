7dba56b93c46e0e6e66ca198f6a985397107f1d0
Othmani Ali <othmanixali@gmail.com>
2018-04-11 14:54:40 +0100

    add underConstruction info in topPlayers cmp

--- src/Components/GameInfo/TopPlayers/TopPlayers.js
+++ src/Components/GameInfo/TopPlayers/TopPlayers.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './TopPlayers.css';
 import TopSVG from './TopSVG/TopSVG';
-
+import UnderConstruction from '../../../Assets/PNGs/under-construction.png';
+//<div className={styles.TopSVG}><TopSVG /></div>
 const topPlayers = (props) => {
     return (
             <div className = {styles.TopPlayers} >
-                <div className={styles.TopSVG}><TopSVG /></div>
+                <div className={styles.UnderConstruction}><img src={UnderConstruction} alt='under constructio' width='200px'/></div>
+                <h3>This section is currently under construction</h3>
             </div>
     );
 };
