8c55407923a46bd3c7b381818e298cbf7939ea30
Othmani Ali <othmanixali@gmail.com>
2018-04-16 16:38:18 +0100

    M addingNewTile for blocker Tiles

--- src/Lib/Functions/AddingNewTile.js
+++ src/Lib/Functions/AddingNewTile.js
@@ -1,19 +1,22 @@
 import print from './Print';
+import pickRandomItems from './pickRandomItems';
 
-const addingNewTile = (matrix, idStore) => {
+const addingNewTile = (matrix, idStore, addBlocker=false) => {
     print('addingNewTile',3);
     let emptytiles =  matrix.map( // collect positions of Empty tiles in a matrix form
-            (matrixRow,row) => matrixRow.reduce(  
+            (matrixRow,row) => matrixRow.reduce(
             (list,tile,col) => !tile.normal.val? list.concat([ [row,col] ]):list, []));
     // transform 'emptyTiles' from a matrix form to an array
     emptytiles = emptytiles.reduce( (array,rowOfEmptytiles) => array.concat(rowOfEmptytiles),[]);
-    // pick a randomcountEmptyTile position from emptyTiles
-    const randomEmptyTile = emptytiles[Math.floor(emptytiles.length*Math.random())] ;
-    const row0 = randomEmptyTile[0], col0 = randomEmptyTile[1];
-    //const M =  Array(4).fill(0).map( (_,row) => Array(4).fill(0).map( (_,col) => Object.assign({}, matrix[row][col]))); 
-    matrix[row0][col0].normal.val = 2;
+    // pick a randomEmptyPositions positions from emptyTiles
+    const rand = pickRandomItems(emptytiles, addBlocker ? 2 : 1);
+    const row0 = rand[0][0], col0 = rand[0][1];
+    if (addBlocker && emptytiles.length) {
+        matrix[rand[1][0]][rand[1][1]].normal.val = 2;
+        matrix[rand[1][0]][rand[1][1]].normal.blocker = 1;}
+    matrix[row0][col0].normal.val = 0;
     matrix[row0][col0].normal.newTile = 1;
-     // add the tile id to idStore and take a new one
+    // add the tile id to idStore and take a new one
     matrix[row0][col0].normal.id = idStore.splice(idStore.length-1, 1, matrix[row0][col0].normal.id)[0];
     return matrix;
  }
