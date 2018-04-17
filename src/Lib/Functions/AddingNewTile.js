655117efa5f0ebfce4143da669501bf494a5333b
Othmani Ali <othmanixali@gmail.com>
2018-04-17 06:40:19 +0100

    M addingNewTile

--- src/Lib/Functions/AddingNewTile.js
+++ src/Lib/Functions/AddingNewTile.js
@@ -8,16 +8,16 @@ const addingNewTile = (matrix, idStore, addBlocker=false) => {
             (list,tile,col) => !tile.normal.val? list.concat([ [row,col] ]):list, []));
     // transform 'emptyTiles' from a matrix form to an array
     emptytiles = emptytiles.reduce( (array,rowOfEmptytiles) => array.concat(rowOfEmptytiles),[]);
-    // pick a randomEmptyPositions positions from emptyTiles
-    const rand = pickRandomItems(emptytiles, addBlocker ? 2 : 1);
+    // pick random empty Tile positions from emptyTiles
+    const rand = pickRandomItems(emptytiles, addBlocker ? 2 : 1); // pick two if we need to add a blocker Tile
     const row0 = rand[0][0], col0 = rand[0][1];
     if (addBlocker && emptytiles.length) {
         matrix[rand[1][0]][rand[1][1]].normal.val = 2;
         matrix[rand[1][0]][rand[1][1]].normal.blocker = 1;}
-    matrix[row0][col0].normal.val = 0;
+    matrix[row0][col0].normal.val = 2;
     matrix[row0][col0].normal.newTile = 1;
-    // add the tile id to idStore and take a new one
-    matrix[row0][col0].normal.id = idStore.splice(idStore.length-1, 1, matrix[row0][col0].normal.id)[0];
+    // add the tile id to idStore and take a new one // why?? 
+    //matrix[row0][col0].normal.id = idStore.splice(idStore.length-1, 1, matrix[row0][col0].normal.id)[0]; // why ?? 
     return matrix;
  }
 
