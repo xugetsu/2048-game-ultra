e0c6649415a0d03fa9aa76dacc232bc68d89e923
Othmani Ali <othmanixali@gmail.com>
2018-04-19 17:08:52 +0100

    M checkForUpdate for Blocker Tiles

--- src/Lib/Functions/CheckForUpdate.js
+++ src/Lib/Functions/CheckForUpdate.js
@@ -1,7 +1,7 @@
 import print from './Print';
 
 // "map" function has been used a lot here 
-// to generalize the use of checkForUpdate function for any size of matrix
+// to generalize the use of checkForUpdate function for any matrix size. 
 
 
 const checkForUpdate = (matrix, direction) => {
@@ -11,8 +11,7 @@ const checkForUpdate = (matrix, direction) => {
     const N = matrix.length;
     // Create a matrix 'M' of tile vals from the matrix 'matrix' :
     let M = matrix.map( matRow => matRow.map( (tile,c) => tile.merged? tile.normal.val*2 : tile.normal.val ));  
-
-    // FTACL : cases of Fixed (inmovable) Tiles Arrangment Code List in base 10 ('0000' '0001' '0011' '0111')
+    // FTACL : cases of Fixed (immovable) Tiles Arrangment Code List in base 10 ('0000' '0001' '0011' '0111')
     const FTACL = [0,1,3,7,15,31,63,127,255,511,1023,2047,4095];
 
     // Modify the position of tiles in M according to 'direction' so that we can always compare tiles values
@@ -37,15 +36,16 @@ const checkForUpdate = (matrix, direction) => {
      // Consequently, each array/row in the matrix will have a binary code based on tiles positions in it (ex '1010'), 
      // The parseInt function will then convert each code to base 10 ( example '1010' becomes '10')
      // so that we can compare them to arrangement codes.
-    const arrangmentCodeMatrix = M.map( matRow => parseInt(matRow.reduce( (str,tileVal)=>str += tileVal? '1': '0' ,''),2) );
-
-
+    const arrangmentCodeMatrix = M.map( matRow => matRow.reduce( (str,tileVal) => {
+                    return str += (tileVal > 1 ? '1': tileVal > 0 ? ' ' : '0')} ,''));
+    let arrangmentCodeList= arrangmentCodeMatrix.reduce( (l,e) => l + ' ' + e, '').split(' ').slice(1);
+    arrangmentCodeList = arrangmentCodeList.map( e => e === '' ? 0 : parseInt(e,2));
 //// 1) Checking if there is at least one tile that can move in the arrangmentCodeMatrix data :
-    for( let r = 0; r < N; r++ ){
+    for( let i = 0; i < arrangmentCodeList.length; i++ ){
         // if the arrangment Code in the actual array/row is not one of the FTACL list codes
         // then there is at least one tile that can move
         // 'checkForUpdate' returns true, the rest of the code/loop will not be executed. 
-        if(!FTACL.includes(arrangmentCodeMatrix[r])){
+        if( !FTACL.includes(arrangmentCodeList[i]) ){
             return true}
     };
 
